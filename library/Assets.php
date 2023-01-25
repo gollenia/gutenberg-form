8d7eacd3ecfeec5d81e629864b170d24de5dd9dc
Thomas Gollenia <thomas@gollenia.at>
2023-01-25 08:03:12 +0100

    correct js names

--- library/Assets.php
+++ library/Assets.php
@@ -1,12 +1,12 @@
 <?php
-namespace Contexis\GutenbergForms;
+namespace Contexis\GutenbergForm;
 
 class Assets {
 
 	public $assets = [
 		'style'         => 'gbf-block',
-		'editor_script' => 'gbf-block-editor',
-		'editor_style'  => 'gbf-block-editor',
+		'editor_script' => 'gbf-form-editor',
+		'editor_style'  => 'gbf-form-style',
 	];
 
 	/**
@@ -21,11 +21,12 @@ class Assets {
 
         add_action( 'init', [$instance, "register_assets"] );
 
-		add_action('enqueue_gfb_form_editor', function() {
-			wp_enqueue_script('gbf-form-editor', plugin_dir_url(__FILE__) . "../assets/admin.js", [], false, true);
+		add_action('admin_enqueue_scripts', function() {
+			wp_enqueue_script('gbf-form-editor');
 		});
 
-       	add_action('init', function() {
+       	add_action('wp_enqueue_scripts', function() {
+			if(is_admin()) return;
         	wp_enqueue_script('gbf-frontend', plugin_dir_url(__FILE__) . "../assets/frontend.js", [], false, true);
     	});
 
@@ -49,17 +50,17 @@ class Assets {
     public function register_assets() {
 		$dir = __DIR__ . "/../assets/";
 
-		if ( ! file_exists( $dir . "index.asset.php" ) ) {
+		if ( ! file_exists( $dir . "backend.asset.php" ) || ! file_exists( $dir . "frontend.asset.php" ) ) {
 			  throw new \Error(
-					  'You need to run `npm start` or `npm run build` for the timber blocks first.'
+					  'You need to run `npm start` or `yarn build` for the gutenberg forms first.'
 			 );
 		}
 
-		$script_asset = require( $dir . "index.asset.php" );
+		$script_asset = require( $dir . "backend.asset.php" );
 
 		wp_register_script(
 			$this->assets['editor_script'],
-			plugins_url( '../assets/index.js', __FILE__ ),
+			plugins_url( '../assets/backend.js', __FILE__ ),
 			$script_asset['dependencies'],
 			$script_asset['version']
 		);
@@ -67,7 +68,7 @@ class Assets {
 
 		wp_register_style(
 			$this->assets['editor_style'],
-			plugins_url( '../assets/index.css', __FILE__ ),
+			plugins_url( '../assets/backend.css', __FILE__ ),
 			array(),
 			$script_asset['version']
 		);
