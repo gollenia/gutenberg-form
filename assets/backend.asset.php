5cede237d357a85801fd8102a4cd6d869b5d60f4
Thomas Gollenia <thomas@gollenia.at>
2023-02-17 17:00:40 +0100

    new build

--- assets/backend.asset.php
+++ assets/backend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd08f0324e416fae6695d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e7018f1492e24c146d16');
