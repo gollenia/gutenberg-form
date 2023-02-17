1522f3c3f443e0b173d604a1fe6e5343fde386c0
Thomas Gollenia <thomas@gollenia.at>
2023-02-17 07:52:49 +0100

    manage submission

--- library/Submit.php
+++ library/Submit.php
@@ -2,30 +2,52 @@
 
 namespace Contexis\GutenbergForm;
 
+use Contexis\GutenbergForm\Mail;
+
 class Submit {
 
+	/**
+	 * Init class
+	 *
+	 * @return void
+	 */
 	public static function init() {
 		$instance = new self();
 		add_action('rest_api_init',array($instance,'register_rest'),10,1);
 	}
 
+	/**
+	 * Register rest route
+	 *
+	 * @return void
+	 */
 	public function register_rest() {
-		register_rest_route( 'gbf-form/v2', '/submit/(?P<id>\d+)', [
-			'method' => 'GET', 
+		register_rest_route( 'gbf-form/v2', 'submit', [
+			'methods' => 'POST', 
 			'callback' => [$this, 'get_rest_data'], 
-			'args' => [
-				'id' => [
-					'validate_callback' => function($param, $request, $key) {
-						return is_numeric( $param );
-					}
-				]
-			],
 			'permission_callback' => '__return_true'
 		], true );
 	}
 
-	public function get_rest_data() {
-
+	/**
+	 * Get data from rest request
+	 *
+	 * @param [type] $request
+	 * @return void
+	 */
+	public function get_rest_data($request) {
+		$result = $request->get_json_params();
+		$id = $result['id'];
+		$formFields = new FormFields($id);
+		$validation = $formFields->validate($result['fields']);
+		$response = new \WP_REST_Response( $validation );
+		$response->set_status( 200 );
+		if(!$validation['success']) return $response;
+		$mail = new Mailer($formFields);
+		$mail->send($id, $result['fields']);
+		
+		return $response;
+		//get get form data by post id
 	}
 
 }
