cb71affb9205babcf1dd95f15193c735bbe6327c
Thomas Gollenia <thomas@gollenia.at>
2023-04-09 07:02:16 +0200

    new bulild

--- assets/backend.asset.php
+++ assets/backend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'cee9670c38dd27c7ee06');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'dabdfd13927557c4299e');
