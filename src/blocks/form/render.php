716e32410d0ba8cf7964db7f703d1de0367fee97
Thomas Gollenia <thomas@gollenia.at>
2024-04-26 14:15:12 +0200

    Add language support to form rendering

--- src/blocks/form/render.php
+++ src/blocks/form/render.php
@@ -1,4 +1,5 @@
 <?php
 $lang = explode("_", get_locale())[0];
+
 $result = "<div class='gbf-form' data-id='" . $attributes['formPost'] . "' data-lang='" . $lang . "' data-page='" . get_the_ID() . "'></div>";
 echo $result;
