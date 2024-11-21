bee3034124abb7b89fb1d0b1a9a3f922886751ac
Thomas Gollenia <thomas@gollenia.at>
2024-11-21 09:02:38 +0100

    reformatted

--- src/plugins/mail-recipients/index.js
+++ src/plugins/mail-recipients/index.js
@@ -7,44 +7,44 @@
  */
 import { CheckboxControl, TextControl } from '@wordpress/components';
 import { select } from '@wordpress/data';
-import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
+import { PluginDocumentSettingPanel } from '@wordpress/editor';
 
 import { useEntityProp } from '@wordpress/core-data';
 import { __ } from '@wordpress/i18n';
 
-const MailRecipients = ( { ...props } ) => {
-	const postType = select( 'core/editor' ).getCurrentPostType();
+const MailRecipients = ({ ...props }) => {
+	const postType = select('core/editor').getCurrentPostType();
 
-	if ( postType !== 'gbf-form' ) {
+	if (postType !== 'gbf-form') {
 		return <></>;
 	}
 
-	const [ meta, setMeta ] = useEntityProp( 'postType', postType, 'meta' );
+	const [meta, setMeta] = useEntityProp('postType', postType, 'meta');
 
 	return (
 		<PluginDocumentSettingPanel
 			name="form-recipient-settings"
-			title={ __( 'Recipients', 'gutenberg-form' ) }
+			title={__('Recipients', 'gutenberg-form')}
 			className="form-recipient-settings"
 		>
 			<TextControl
-				label={ __( 'Mail addresses', 'gutenberg-form' ) }
-				help={ __(
+				label={__('Mail addresses', 'gutenberg-form')}
+				help={__(
 					'Separate multiple addresses with commas',
 					'gutenberg-form'
-				) }
-				value={ meta._mail_recipients }
-				onChange={ ( value ) => {
-					setMeta( { _mail_recipients: value } );
-				} }
+				)}
+				value={meta._mail_recipients}
+				onChange={(value) => {
+					setMeta({ _mail_recipients: value });
+				}}
 			/>
 
 			<CheckboxControl
-				label={ __( 'Send copy to admin', 'gutenberg-form' ) }
-				checked={ meta._send_to_admin }
-				onChange={ ( value ) => {
-					setMeta( { _send_to_admin: value } );
-				} }
+				label={__('Send copy to admin', 'gutenberg-form')}
+				checked={meta._send_to_admin}
+				onChange={(value) => {
+					setMeta({ _send_to_admin: value });
+				}}
 			/>
 		</PluginDocumentSettingPanel>
 	);
